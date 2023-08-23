47f44946d0de406ee5b7373bf9126fd46f9a3392
morriswa <william@morriswa.org>
2023-08-23 13:35:34 -0500

    set up mvc unit testing

---
+++ src/test/java/org/morriswa/messageboard/HealthEndpointsTest.java
@@ -0,0 +1,54 @@
+package org.morriswa.messageboard;
+
+import org.apache.http.HttpHeaders;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.morriswa.messageboard.config.TestSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = {TestSecurityConfig.class})
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class HealthEndpointsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Environment e;
+
+    @Test
+    void testHealthEndpoint() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                    .get("/"+e.getRequiredProperty("common.service.endpoints.health.path")))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath(
+                        "$.message",
+                        Matchers.equalTo(e.getRequiredProperty("common.service.endpoints.health.messages.get"))));
+    }
+
+    @Test
+    void testSecureHealthEndpoint() throws Exception {
+
+        final String targetUrl = String.format("/%s%s",
+                e.getRequiredProperty("server.path"),
+                e.getRequiredProperty("common.service.endpoints.health.path"));
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get(targetUrl)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+            .andExpect(status().is(200))
+            .andExpect(jsonPath(
+                    "$.message",
+                    Matchers.equalTo(e.getRequiredProperty("common.service.endpoints.health.messages.get"))));
+    }
+}
